fadc84a31144394ea5251cdf88bbe0d1fc19781f
Marco Toniut <marco.toniut@gmail.com>
2022-01-02 12:15:10 -0300

    Add `prettier` config, `format script` and run it

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  arrowParens: "always",
+  overrides: [],
+};
